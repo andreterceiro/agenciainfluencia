d6b552b4a85c6c2e79b4eb03f78a4ec5ab219d1d
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2020-05-22 05:49:23 -0300

    Inclusão de um arquivo para funções genéricas

---
+++ lib/funcoes.php
@@ -0,0 +1,43 @@
+<?php
+function emptyArray(array $array)
+{
+    foreach ($array as $item) {
+        if (! empty($item)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function emptyMysqliResult(mysqli_result $mysqliresult)
+{
+    if ($mysqliresult->num_rows == 0 ) {
+        return true;
+    }
+
+    return false;
+}
+
+function carregarModel($model)
+{
+    global $diretorioLib;
+
+    require_once $diretorioLib . "/../model/" . $model . "Model.php";
+}
+
+function erroStringParaAlert($erros)
+{
+    $erroEncontrado = '';
+
+    foreach ($erros as $erro) {
+         if (! is_null($erro)) {
+             $erroEncontrado .= $erro . "\\n";
+         }
+    }
+
+    if (! empty($erroEncontrado)) {
+        return mb_substr($erroEncontrado, 0, -2);
+    }
+
+    return '';
+}
