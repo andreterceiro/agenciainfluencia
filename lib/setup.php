c717cfb47892e66da2394d033eb320be4f4f5f8a
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2020-05-22 08:21:12 -0300

    Adicionando as views de layout no setup.php

--- lib/setup.php
+++ lib/setup.php
@@ -1,20 +1,26 @@
 <?php
 session_start();
 
+global $baseView;
 global $registry;
+
 $registry->set('view/erro500', 'erro/500.phtml');
 $registry->set('apiKeyNis', '9d99f2b191c619bdabef5c054c996843');
 
 if (! extension_loaded('mbstring')) {
-    global $baseView;
-
     $registry->set('erro', 'É necessário carregar a extensão mbstring');
     $baseView->render("/erro/500");    
 }
 
 if (! extension_loaded('curl')) {
-    global $baseView;
-
     $registry->set('erro', 'É necessário carregar a extensão curl');
     $baseView->render("/erro/500");    
 }
+
+$baseView->set(
+    "layout",
+     array(
+        "cabecalho" => "/layout/cabecalho",
+        "rodape" => "/layout/rodape"
+    )
+);
