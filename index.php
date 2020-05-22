d88a105678d66d6d4199e7e6502fdd2cc4070197
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2020-05-22 05:44:53 -0300

    Ajuste do index, com o fluxo da aplicação

--- index.php
+++ index.php
@@ -1,3 +1,52 @@
 <?php
-require_on
+require_once('lib/loader.php');
 
+carregarModel('Cliente');
+$cliente = new ClienteModel;
+
+if ((isset($_POST['cmdEnviar'])) && $_POST['cmdEnviar'] == "Enviar") {
+    try {
+        $erro[] = $cliente->setNome($_POST['nome']);
+        $cliente->setNis($_POST['nis']);
+        $erro[] = $cliente->setTelefone($_POST['telefone']);
+        $erro[] = $cliente->setEmail($_POST['email']);
+        $erro[] = $cliente->gravar($_POST);
+    	
+        if (emptyArray($erro)) {
+            $baseView->set(
+                "clientes",
+   	        $cliente->consultarTodos()
+            );
+            $baseView->render("/clientes/listagem");
+        } else {
+            $baseView->set(
+                "erro",
+   	        $erro
+            );
+            
+            $baseView->set(
+                "dadosPostados",
+   	        $_POST
+            );
+
+            $baseView->render("/clientes/formulario");
+        }
+    } catch (Exception $e) {
+        $baseView->set("erro", $e->getMessage());
+        $baseView->render("/clientes/formulario");
+    }
+} elseif ( (isset($_POST['cmdEnviar'])) && ($_POST['cmdEnviar'] == "Cadastrar") )  {
+        $baseView->render("/clientes/formulario");
+} elseif ( (isset($_GET['id'])) && (! is_null ($_GET['id'])))  {
+    $baseView->set(
+        "cliente",
+	$cliente->consultar((int) $_GET['id'])
+    );
+    $baseView->render("/clientes/detalhes");
+} else {
+    $baseView->set(
+        "clientes",
+	$cliente->consultarTodos()
+    );
+    $baseView->render("/clientes/listagem");
+}
