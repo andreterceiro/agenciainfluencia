2273e872f6bb313b68c02a3e483ea3c8d272c56e
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2020-05-22 05:47:54 -0300

    Ajuste do loader

--- lib/loader.php
+++ lib/loader.php
@@ -1,5 +1,18 @@
 <?php
-require_once "Conexao.php";
-//require_once "../lib/Registry.php";
+chdir(__DIR__);
+$diretorioLib = getcwd();
 
+require_once $diretorioLib . "/Registry.php";
+$registry = new Registry;
+
+require_once $diretorioLib . "/Conexao.php";
 $conexao = new Conexao;
+
+require_once $diretorioLib . "/error.php";
+
+require_once $diretorioLib . "/../view/BaseView.php";
+$baseView = new BaseView;
+
+require_once $diretorioLib . "/setup.php";
+
+require_once $diretorioLib . "/funcoes.php";
